335a1f721f8f734b6993e4cf9d29c5d9653eb8e2
Zaenul Hilmi <zaenulhilmi@gmail.com>
2022-05-11 02:59:25 +0700

    adding shortcut to rust cargo run

--- lua/alt_number_shortcut.lua
+++ lua/alt_number_shortcut.lua
@@ -3,12 +3,16 @@ vim.api.nvim_set_keymap('n', '<M-1>', ':NERDTreeToggle<CR>', {})
 vim.api.nvim_create_augroup("AltNumberShortcuts", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
 	group = "AltNumberShortcuts",
-	pattern = { "lua", "go" },
+	pattern = { "lua", "go", "rust" },
 	callback = function()
 		local file_name = vim.fn.expand("<afile>")
 		local match = vim.fn.expand("<amatch>")
+        print(file_name, match)
+
 		if match == "go" then
 			vim.api.nvim_set_keymap('n', '<M-8>', '<cmd>!go run ./... <CR>', { noremap = true, silent = true, expr = false })
+		elseif match == "rust" then
+			vim.api.nvim_set_keymap('n', '<M-8>', '<cmd>!cargo run <CR>', { noremap = true, silent = true, expr = false })
 		elseif match == "lua" then
 			vim.api.nvim_set_keymap('n', '<M-8>', '<cmd>!lua ' .. file_name .. ' <CR>', { noremap = true, silent = true, expr = false })
 		end
