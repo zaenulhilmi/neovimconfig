e3c4a621b7c52ee89b2c678e42ade1981265b1fa
Zaenul Hilmi <zaenulhilmi@gmail.com>
2022-05-17 09:00:24 +0700

    adding no swapfile

--- lua/option.lua
+++ lua/option.lua
@@ -5,12 +5,13 @@ vim.go.hidden = true
 vim.go.incsearch = true
 vim.go.hlsearch = false
 vim.go.clipboard = "unnamedplus"
-vim.bo.swapfile = false
 vim.wo.number = true
 vim.wo.wrap = false
 vim.wo.scrolloff = 8
 vim.wo.signcolumn = "yes"
 vim.wo.colorcolumn = "80"
+-- vim.bo.swapfile = false
+vim.cmd[[set noswapfile]]
 vim.cmd[[cabbrev h vert bo h]]
 vim.cmd([[let g:mapleader = " "]])
 vim.cmd([[set tabstop=4 softtabstop=4 shiftwidth=4 expandtab smartindent smarttab]])
